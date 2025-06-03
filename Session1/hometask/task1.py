ebb3c418de702dc0c4d96382de7652450e891753
Elsu Bektemir <elsubektemirova@gmsil.com>
2025-06-03 19:58:46 +0000

    Practice

---
+++ Session1/hometask/task1.py
@@ -0,0 +1,70 @@
+# print("This program prints Hello, World")
+# print( 'print("Hello, World")' )
+# help("keywords")
+
+# Exercise 1. Basic Arithmetic Operations 
+# Write a program that takes two integers from the 
+# user and prints the result of addition, subtractiom, multipliation,and division of these two numbers 
+
+# num = int(input("Please provide a number : "))
+# num2 = int (input("Please provide a second number : "))
+# print( num + num2 )
+# print( num - num2 )
+# print( num * num2 )
+# print( num / num2 )
+
+# Task 2 . 
+
+# Given the string sentence = " Python programminf is fun!"
+# Print the substing " programing " using sliccing. 
+# Print every 2nd charachter from the string 
+# Reverse the string and print it.
+
+# word= "Pythin programing is fun !"
+# print(word[6:18])
+# print(word[::2])
+# print(word[::-1])
+
+# Task 3. 
+
+# Take two strings as input: first name and last name. 
+# Concatenate  them with a space in between and print the full name.
+# Print the full name in all uppercase letters.
+
+# first = "Elsu"
+# last = "Loda"
+# print(first.upper(),last.upper(), end='\n', sep=' has her granfathers last name with is ')
+
+# Task 4. 
+# Ask the user ti input a sentence, then print the length of that sentence (number of characrters )
+
+# sen = input(" Please provide a sentence: ")
+# print(len(sen))
+
+# Task 5 
+# Take two integers as input.
+# Print the results of integer division (//).
+# Print the remainder using modulus operator.
+
+# int1 = int(input("Please provide number: "))
+# int2 = int(input("Please provide number 2: "))
+# print( int1//int2)
+# print ( int1%int2)
+
+# Task 6
+# Write a program to convert a temperature from Celsius to Fahrenheit.
+# Formula: F = ( C*9/5)+32 
+# Take input temperature in Celsius ans output the temperature in Farenheir. 
+
+# cel = float(input(" Please provide celsius: "))
+# print((cel*9/5)+32)
+
+# Task 7 
+# Take two integers, base and exponent, as input 
+# Print the results of base raised to the power of exponent (**)
+# Print the modulus of base by expondent (%) 
+ 
+# base = int(input("Please provide base: ")) 
+# exponent = int(input("Please provide base : ") )
+# print(base**exponent)
+# print(base%exponent)
