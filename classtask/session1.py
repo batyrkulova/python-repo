ad8728a78c5094bec92ec15787276f0e70ddc6f0
Elsu Bektemir <elsubektemirova@gmsil.com>
2025-06-03 17:04:59 +0000

    Notes

---
+++ classtask/session1.py
@@ -0,0 +1,112 @@
+#Numeric data types
+# Both of the numeric date types can hold negative numbers 
+
+#Integer
+#Whole Numbers
+#Example : 1,10,20, -10 
+
+## Float
+#Decimal Point number:
+#example: 2.1 , 4.6, 6.0, -8.5, 5.7474
+# python3 filename 
+
+# integer_number = 10
+# float_number = 5.6
+# task_val = 7.8
+# print(integer_number, float_number, task_val)
+
+#addition
+#subtraction
+#multiplication
+#division 
+
+# print(5*10.5 ) #multiplication
+# print(50/10.5 ) #division
+# print(50+10.5 ) #addition
+# print(50-10.5 ) #subtraction
+
+# print (10** 2) #10 power of 2
+# # 10 * 10  if 3 then 10*10*10 
+# print (10%2) modules/remainder
+# #10/2 = 5 5 is a whole number
+# #11/2 = 5.5 is a float number
+# #11%2 = the closest number 10 
+
+#string
+#string data is a text /word
+
+
+
+# sentence = "This is python 1. This is sentence 2. This is sentence 3 ...."
+# mutliple_str = '''This is line 1 
+# This is line 2
+# This is line 3
+# '''
+# print(mutliple_str)
+
+#String indexing 
+#Indexing always starts with 0
+#You can go from the back of the string the index starting -1
+# word = "Hello world "
+#String slicing
+#start location | end location
+
+# print(word[6:11])
+
+# print(word[::2])
+
+#Reverse the wordl
+# print(word[::-1])
+
+#String Concatiation
+#Joining 2 or more strings together 
+#default values for end = '\n' and sep ' '
+# st1 = "akumo"
+# st2 = " Sol"
+# # # joined = st1 + st2
+# # print ( st1, st2, end='\n', sep=' This is a seperation' )
+# # print ("This is the same line")
+
+# print(" This is 1 .\nThis is 2")
+
+#len(<string>)--> calculates the length of the string | Counts from 1 and how may characters
+
+# word = "Hello there, how are you doing "
+
+# print(len(word))
+
+#Iput function always takes string input 
+# int() converts data type to integer (if it's correct ) It's a fuction--> integer
+#float() float 
+# inp = int(input("Please provide a number 1 "))
+# inp2 = float(input("Please provide number 2 " ))
+# print(inp + inp2)
+# / -> Float division
+# // -> Integer division 
+
+# Class task 
+# inp = int(input("Please provide a number 1 "))
+# inp2 = int(input("Please provide number 2 " ))
+# print ( inp * inp2 )
+
+# inp = int(input("Please provide a number 1 "))
+# inp2 = int(input("Please provide number 2 " ))
+# print ( inp / inp2 )
+
+# inp = int(input("Please provide a number 1 "))
+# inp2 = int(input("Please provide number 2 " ))
+# print ( inp % inp2 )
+
+# inp = input("Please provide a number " )
+
+# print(len(inp))
+
+# inp = ( 37 * 9/5) +32 
+# print(inp)
+
+
+
+
+
+
+
