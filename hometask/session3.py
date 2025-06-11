e7011b69cf3c045e1484e1a2edaa27a9e52b52e9
Batyrkulova <Batyrkulova>
2025-06-11 18:58:40 +0000

    Final version

--- hometask/session3.py
+++ hometask/session3.py
@@ -1,36 +1,36 @@
-#Task1
+Task1:
 
-# inp = input("please enter an input: ").split()
-# reversed_list = []
-# for i in reversed(inp):
-#     if i not in reversed_list:
-#         reversed_list.append(i)
-# print(reversed_list)
+inp = input("please enter an input: ").split()
+reversed_list = []
+for i in reversed(inp):
+    if i not in reversed_list:
+        reversed_list.append(i)
+print(reversed_list)
 
-# # # Task 2.
-# words = input("Enter words: ").split()
-# duplicate = []
-# for word in words :
-#     if word not in duplicate:
-#         duplicate.append(word)
+# # Task 2:
+words = input("Enter words: ").split()
+duplicate = []
+for word in words :
+    if word not in duplicate:
+        duplicate.append(word)
        
-# print(duplicate)
+print(duplicate)
 
 
-# Task 3.
-# inp = input("Please enter words: ").split()
-# longest = max(inp, key=len)
-# print(longest)
+Task 3:
+inp = input("Please enter words: ").split()
+longest = max(inp, key=len)
+print(longest)
 
-# inp = input("Please enter words: ").split()
-# longest = ""
-# for word in inp:
-#     if len(word) > len(longest):
-#         longest = word
-# print(longest)
+inp = input("Please enter words: ").split()
+longest = ""
+for word in inp:
+    if len(word) > len(longest):
+        longest = word
+print(longest)
 
 
-# Task 4:
+Task 4:
 numbers = list(map(int, input("Enter numbers: ").split()))
 larg = 0
 secd = 0
